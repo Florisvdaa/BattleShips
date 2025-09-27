e20eac3acdb4ff5e23483cbebf013abec75134fc
FlorisvanderAa <florisyt2@gmail.com>
2025-09-27 17:45:32 +0200

    Placement preview shader implemented
    User can now see the preview of the object to place using a nice trarnsparent shader.

--- Assets/Scripts/Systems/PlacementSystem.cs
+++ Assets/Scripts/Systems/PlacementSystem.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField] private GameObject mouseIndicator;
-    [SerializeField] private GameObject cellIndicator;
     [SerializeField] private InputManager inputManager;
 
     [SerializeField] private Grid grid;
@@ -13,19 +12,18 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private int selectedObjectIndex = -1;
 
     [SerializeField] private GameObject gridVisualization;
+    [SerializeField] private PreviewSystem previewSystem;
 
     private GridData gridData, boatData;
-    private Renderer previewRenderer;
     private List<GameObject> placedObjects = new();
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
 
     private void Start()
     {
-        Debug.Log("Start");
-
         StopPlacement();
         gridData = new();
         boatData = new();
-        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
+
     }
 
     public void StartPlacement(int ID)
@@ -38,7 +36,7 @@ public class PlacementSystem : MonoBehaviour
             return;
         }
         gridVisualization.SetActive(true);
-        cellIndicator.SetActive(true);
+        previewSystem.StartShowingPlacementPreview(objectsDatabase.objectsData[selectedObjectIndex].Prefab, objectsDatabase.objectsData[selectedObjectIndex].Size);
         inputManager.OnClicked += PlaceStructure;
         inputManager.OnExit += StopPlacement;
     }
@@ -47,9 +45,10 @@ public class PlacementSystem : MonoBehaviour
     {
         selectedObjectIndex = -1;
         gridVisualization.SetActive(false);
-        cellIndicator.SetActive(false);
+        previewSystem.StopShowinPreview();
         inputManager.OnClicked -= PlaceStructure;
         inputManager.OnExit -= StopPlacement;
+        lastDetectedPosition = Vector3Int.zero;
     }
 
     private void PlaceStructure()
@@ -71,6 +70,8 @@ public class PlacementSystem : MonoBehaviour
         placedObjects.Add(newGO);
         GridData selectedData = objectsDatabase.objectsData[selectedObjectIndex].ID == 0 ? gridData : boatData;
         selectedData.AddObjectAt(gridPos, objectsDatabase.objectsData[selectedObjectIndex].Size, objectsDatabase.objectsData[selectedObjectIndex].ID, placedObjects.Count - 1);
+
+        previewSystem.UpdatePosition(grid.CellToWorld(gridPos), false); 
     }
 
     private bool CheckPlacementValidity(Vector3Int gridPos, int selectedObjectIndex)
@@ -87,10 +88,13 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePos = inputManager.GetSelectedMapPosition();
         Vector3Int gridPos = grid.WorldToCell(mousePos);
 
-        bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex);
-        previewRenderer.material.color = placementValidity ? Color.green : Color.red;
+        if (lastDetectedPosition != gridPos)
+        {
+            bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex);
 
-        mouseIndicator.transform.position = mousePos;   
-        cellIndicator.transform.position = grid.CellToWorld(gridPos);
+            mouseIndicator.transform.position = mousePos;
+            previewSystem.UpdatePosition(grid.CellToWorld(gridPos), placementValidity);
+            lastDetectedPosition = gridPos;
+        }
     }
 }
